3548397eaf3a8008f9b02cb1da5f8847a93b5570
Geraldo Neto <geraldospn26@gmail.com>
2025-11-18 14:40:32 -0300

    feat: add new class Hero

--- Heroi.cs
+++ Heroi.cs
@@ -7,6 +7,35 @@ namespace Escrevendo_as_classes_de_um_Jogo
 {
     public class Heroi
     {
-        
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Type { get; set; }
+
+        public Heroi(string name, int age, string type)
+        {
+            Name = name;
+            Age = age;
+            Type = type;
+        }
+
+        public string Attack()
+        {
+            if (Type.ToLower() == "mage")
+            {
+                return "Attack with magic";
+            }
+            else if (Type.ToLower() == "warrior")
+            {
+                return "Attack with sword";
+            }
+            else if (Type.ToLower() == "ninja")
+            {
+                return "Attack with shuriken";
+            }
+            else
+            {
+                return "Attack";
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Escrevendo_as_classes_de_um_Jogo;
+
+Heroi superman = new Heroi("Geraldo", 22, "mage");
+
+
+
+Console.WriteLine($"The {superman.Type} {superman.Attack()}");
