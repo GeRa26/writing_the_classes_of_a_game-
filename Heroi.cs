e6de748483bcf8f6e2d4e50e10a718d218c2c7af
Geraldo Neto <geraldospn26@gmail.com>
2025-11-18 13:26:06 -0300

    First Commit

---
+++ Heroi.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escrevendo_as_classes_de_um_Jogo
+{
+    public class Heroi
+    {
+        
+    }
+}
